024fa1d40470d6011696f62567359507f134e5b4
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-10-23 17:20:53 +0100

    ✨ feat(proj1): Implement core functionality for LinkedListDeque
    - Added methods to add items at the beginning and end (addFirst, addLast).
    - Implemented methods to remove items from the beginning and end (removeFirst, removeLast).
    - Introduced utility methods: isEmpty, size, and printDeque.

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -26,4 +26,68 @@ public class LinkedListDeque<T> {
     public LinkedListDeque(){
         sentinel = new IntNode(null);
     }
+
+    public  void addFirst(T item){
+        size += 1;
+        IntNode firstNode = new IntNode(item);
+        firstNode.prev = sentinel;
+        firstNode.next = sentinel.next;
+        firstNode.next.prev = firstNode;
+
+        sentinel.next = firstNode;
+        if (sentinel.prev == sentinel){
+            sentinel.prev = firstNode;
+        }
+    }
+
+    public void addLast(T item){
+        size += 1;
+        IntNode lastNode = new IntNode(item);
+        lastNode.next = sentinel;
+        lastNode.prev = sentinel.prev;
+        lastNode.prev.next = lastNode;
+
+        sentinel.prev = lastNode;
+        if (sentinel.next == sentinel){
+            sentinel.next = lastNode;
+        }
+    }
+
+    public T removeFirst(){
+        T removedItem = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        if (size > 0){
+            size += -1;
+        }
+        return removedItem;
+    }
+
+    public T removeLast(){
+        T removedItem = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        if (size > 0){
+            size += -1;
+        }
+        return removedItem;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        IntNode p = sentinel.next;
+        while (p != sentinel){
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
 }
