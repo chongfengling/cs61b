01bbce6186102f794a1acc3db30416e929d45b61
Chongfeng Ling <ling.chongfeng@outlook.com>
2024-03-28 12:47:11 +0000

    implement Dog class

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,13 +1,14 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Chongfeng Ling
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = join(".capers", "dogs");
@@ -38,8 +39,7 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        return readObject(join(DOG_FOLDER, name), Dog.class);
     }
 
     /**
@@ -55,7 +55,16 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File f = join(DOG_FOLDER, this.name);
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        writeObject(f, this);
+
     }
 
     @Override
