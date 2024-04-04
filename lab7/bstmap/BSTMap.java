cf7cd5151a221682a013d6900ec6ffe84c7bc0ec
Chongfeng Ling <ling.chongfeng@outlook.com>
2024-04-04 13:54:41 +0100

    ✨ feat(BSTMap): implement initial structure

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,72 @@
+package bstmap;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparator<K>, V> implements Map61B<K, V> {
+
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void put(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void printInOrder() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
